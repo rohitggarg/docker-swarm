6e1589212eabe41d29e3007b1e33823b149a8314
RohitG <rohit_garg-xft@external.mckinsey.com>
2017-05-30 09:47:02 +0100

    Added configuration option to change the docker bip

---
+++ examples/create_portainer.sh
@@ -0,0 +1,8 @@
+docker service create \
+    --name portainer \
+    --publish mode=host,target=9000,published=9000 \
+    --mode global \
+    --constraint 'node.role == manager' \
+    --mount type=bind,src=/root/.docker,dst=/certs \
+    portainer/portainer \
+    -H tcp://10.195.0.1:2376 --tlsverify
