ca4bc8fe80f46d997977c46253a5c05a5bec03cc
RohitG <rohit_garg-xft@external.mckinsey.com>
2017-06-09 12:26:57 +0100

    Added section to assign type variable to the participating hosts

--- examples/create_portainer.sh
+++ examples/create_portainer.sh
@@ -2,7 +2,7 @@ docker service create \
     --name portainer \
     --publish mode=host,target=9000,published=9000 \
     --mode global \
-    --constraint 'node.role == manager' \
+    --constraint 'node.labels.type == bootstrap' \
     --mount type=bind,src=/root/.docker,dst=/certs \
     portainer/portainer \
     -H tcp://10.195.0.1:2376 --tlsverify
