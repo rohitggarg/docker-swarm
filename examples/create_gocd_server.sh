f606ed0f678a302db8817622cde77081974f4ffe
RohitG <rohit_garg-xft@external.mckinsey.com>
2017-09-13 11:14:33 +0100

    fixing issue with port expose

--- examples/create_gocd_server.sh
+++ examples/create_gocd_server.sh
@@ -1,6 +1,6 @@
 docker service create \
     --name gocd-server \
-    --publish mode=host,target=10000,published=8153 \
+    --publish mode=host,target=8153,published=10000 \
     --mode global \
     --detach=false \
     --constraint 'node.labels.type == bootstrap' \
